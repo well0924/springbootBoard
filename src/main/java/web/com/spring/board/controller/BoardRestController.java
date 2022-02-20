5cbee2d01421fa2b8f74d43f395fb26eca9dda47
well0924 <well414965@gmail.com>
2022-02-20 19:03:29 +0900

    검색쿼리 및 컨트롤러 소스 리팩토링

--- src/main/java/web/com/spring/board/controller/BoardRestController.java
+++ src/main/java/web/com/spring/board/controller/BoardRestController.java
@@ -18,6 +18,7 @@ import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import web.com.spring.board.service.BoardService;
 import web.com.spring.board.vo.BoardVO;
+import web.com.spring.common.Criteria;
 
 @Slf4j
 @RestController
@@ -25,20 +26,13 @@ import web.com.spring.board.vo.BoardVO;
 @RequestMapping("/api/*")
 public class BoardRestController {
 	
-	private BoardService service;
+	private final BoardService service;
 	
 	//게시글 목록 c.o
 	@GetMapping("/list")
-	public ResponseEntity<List<BoardVO>>boardList()throws Exception{
-		log.info("글 목록");
+	public ResponseEntity<List<BoardVO>>boardList(Criteria cri)throws Exception{
 		List<BoardVO>listboard = new ArrayList<>();
-		try {
-			listboard = service.boardlist();
-		} catch (Exception e) {
-			// TODO: handle exception
-			e.printStackTrace();
-		}
-		
+		listboard = service.boardlist(cri);
 		return new ResponseEntity<>(listboard,HttpStatus.OK);
 	}
 	
@@ -47,43 +41,19 @@ public class BoardRestController {
 	public ResponseEntity<String>boardwrite(@RequestBody BoardVO vo)throws Exception{
 		
 		int result = service.boardInsert(vo);
-		return result == 1 ? 
-				new ResponseEntity<>("success",HttpStatus.OK) : 
-				new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+		return result == 1 ? new ResponseEntity<>("success",HttpStatus.OK) : new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
 	}
 	//게시글 수정 c.o
 	@PutMapping("/board/{id}")
 	public ResponseEntity<String>boardUpdate(@PathVariable("id") Integer boardId,@RequestBody BoardVO vo)throws Exception{
-		log.info("글수정");
-		try {
-			int result = service.boardUpdate(vo);
-			
-			if(result >0) {
-				System.out.println("c.o");
-			}else {
-				System.out.println("n.k");
-			}
-		} catch (Exception e) {
-			// TODO: handle exception
-			e.printStackTrace();
-		}
-		return new ResponseEntity<>(HttpStatus.OK);
+			int result = service.boardUpdate(vo);			
+		return result ==1 ? new ResponseEntity<>(HttpStatus.OK): new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
 	}
 	//게시글 삭제 c.o
 	@DeleteMapping("/board/{id}")
 	public ResponseEntity<String>boardDelete(@PathVariable("id") Integer boardId)throws Exception{
-		log.info("글 삭제");
-		try {
-			int result = service.boardDelete(boardId);
-			
-			if(result > 0) {
-				System.out.println("c.o");
-			}else {
-				System.out.println("n.k");
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+		int result = service.boardDelete(boardId);
+		
 		return new ResponseEntity<>(HttpStatus.OK);
 	}
 }
